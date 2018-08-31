d26e1f6d850a6410a1a6d8bcf4b563628709d62e
HuaLouChuQiu <1015110571@qq.com>
2018-08-31 14:41:19 +0800

    aa

--- news/statics/js/team_detail.js
+++ news/statics/js/team_detail.js
@@ -1,12 +1,13 @@
 $(function(){
-    function GetQueryString(name)
+    function getUrlParam(name)
     {
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if(r!=null)return unescape(r[2]); return null;
-    }
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r!=null) return unescape(r[2]); return null; //返回参数值
+    } 
     
-    var id = GetQueryString(id);
+    var id = getUrlParam('id');
+    console.log(getUrlParam('id'))
     console.log(id);
     $.ajax({
         type: 'GET',
