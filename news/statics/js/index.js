2d2ae677695c70afe43f48a1e8201dc3dcee7633
HuaLouChuQiu <1015110571@qq.com>
2018-09-03 10:30:14 +0800

    sss

--- news/statics/js/index.js
+++ news/statics/js/index.js
@@ -42,4 +42,32 @@ $(function(){
         }
         
       });
+      $.ajax({
+        type: 'GET',
+        url: '../../../news/showNewsByClass1',
+        dataType: 'JSON',
+        success: function(data){
+            console.log(data);
+            for(var i=0;i<data.length;i++){
+                var html = 
+                `
+                <li  class="serviceitem wow">
+                <a href="#" target="_blank">
+                    <p class="service_img"><img src="statics/images/148100202198.jpg" width="320" height="120" alt="战略规划管理咨询" /></p>
+                    <div class="service_info">
+                        <p class="title">战略规划管理咨询</p>
+                        <p class="description">从定价策略到品牌定位，再到数字化营销。我们丰富的经验确保定制方案，市场营销是在创造/沟通交换产品中... </p>
+                    </div>
+                </a>
+                <a href="#" target="_blank" class="details">more<i class="fa fa-angle-right"></i></a>
+            </li>
+                `
+                $('#headlineNews').append(html);
+            }
+        },
+        error:function(error){
+            console.log(error);
+        }
+        
+      });
 })
