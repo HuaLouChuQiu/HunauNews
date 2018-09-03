e7a94c455cdafabab56ed76d1a093dda7d46fed1
HuaLouChuQiu <1015110571@qq.com>
2018-09-03 11:04:54 +0800

    aa

--- news/statics/js/index.js
+++ news/statics/js/index.js
@@ -52,12 +52,12 @@ $(function(){
             for(var i=0;i<data.length;i++){
                 let html = 
                 `
-                <li class="serviceitem wow">
+                <li id="serviceitem_0" class="serviceitem wow">
                 <a href="#" target="_blank">
-                    <p class="service_img"><img src="statics/images/news (71).png" width="320" height="120" alt="战略规划管理咨询" /></p>
+                    <p class="service_img"><img src="statics/images/news (73).png" width="320" height="120" alt="战略规划管理咨询" /></p>
                     <div class="service_info">
-                        <p class="title">战略规划管理咨询</p>
-                        <p class="description">从定价策略到品牌定位，再到数字化营销。我们丰富的经验确保定制方案，市场营销是在创造/沟通交换产品中... </p>
+                        <p class="title">${data[i].title}</p>
+                        <p class="description">${data[i].text}</p>
                     </div>
                 </a>
                 <a href="#" target="_blank" class="details">more<i class="fa fa-angle-right"></i></a>
