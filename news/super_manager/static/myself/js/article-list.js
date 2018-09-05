d89c2bb68ea13d907a0589b426f95fb7e5e37142
HuaLouChuQiu <1015110571@qq.com>
2018-09-05 14:40:04 +0800

    ss

--- news/super_manager/static/myself/js/article-list.js
+++ news/super_manager/static/myself/js/article-list.js
@@ -53,11 +53,11 @@ $(function () {
                 $('#userTbody').empty();
                 $('.pagination').empty();
                 console.log(data);
-                if(data=""){
+                if(data == ""){
                     $('.odd').find('.dataTables_empty').css('display:block')
                 }else{
                     // $('.odd').find('.dataTables_empty').css('display:none')
-                    
+
                     console.log(data.length);
                     for(var i=0;i<data.length;i++){
                         var oDate = new Date(data[i].create_time);
