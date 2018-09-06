0f6f758ea61cc3d97b6b1e98ad5a1ccd4d4b502c
HuaLouChuQiu <1015110571@qq.com>
2018-09-06 12:12:17 +0800

    搜索

--- news/statics/js/apply.js
+++ news/statics/js/apply.js
@@ -4,7 +4,7 @@ $(function(){
         let html = 
         `
         <a href="javascript:void(0);" target="_self">
-        <span style="backgroud:#2495e4;color:#fff" data-title="已申请">已申请</span>
+        <span style="color:#2495e4" data-title="已申请">已申请</span>
         <i class="fa fa-angle-right"></i>
     </a>
         `
